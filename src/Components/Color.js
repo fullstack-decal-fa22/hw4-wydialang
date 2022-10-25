9a45a362fe59248bf81254c9d9ecab82f1e3080e
Lydia W <lydiaywang@berkeley.edu>
2022-10-24 22:36:49 -0700

    translated class component to functional component

--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-/* TODO: Translate the below class component to a functional component! */
+/* TODO: Translate the below class component to a functional component! 
 class Color extends React.Component {
 
     constructor(props) {
@@ -10,6 +10,11 @@ class Color extends React.Component {
     render() {
         return <button onClick={() => this.props.handleClick(this.props.color)}>Post {this.props.color}</button>
     }
+}
+*/
+
+const Color = (props) => {
+    return (<button onClick={() => props.handleClick(props.color)}>Post {props.color}</button>);
 }
 
 export default Color;
